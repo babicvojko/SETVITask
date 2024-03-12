fadb36ff8528c6a2ab92959eef2d72efe42420fc
Babic Vojko <vojko.babic@cubes.rs>
2024-03-12 10:23:17 +0100

    Initial project

---
+++ SETVITask/SETVITaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SETVITaskApp.swift
+//  SETVITask
+//
+//  Created by Babic Vojko on 12.3.24..
+//
+
+import SwiftUI
+
+@main
+struct SETVITaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
